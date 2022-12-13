abd539b80e1a013558598de2549e533528eacec5
YouriVarossieau <yourivarossieau@gmail.com>
2022-12-13 14:52:30 +0100

    samengevoegd

--- controlfunction.js
+++ controlfunction.js
@@ -4,7 +4,7 @@ function checkPassword() {
     var password = document.getElementById("inputWachtwoord1").value;
 
     // Check if the password is valid
-    if (inputWachtwoord2 == inputWachtwoord1) {
+    if (inputWachtwoord2 === inputWachtwoord1) {
       // If the password is valid, do something (e.g. display a message or enable a button)
       document.getElementById("result").innerHTML = "";
     } else {
