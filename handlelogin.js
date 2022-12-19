180dd5e28bd3a16eb7357984a1777cacb83a540c
TDRAGSTRA <TIMBO040@GMAIL.COM>
2022-12-19 19:10:20 +0100

    changed urls

--- handlelogin.js
+++ handlelogin.js
@@ -1,36 +1,36 @@
 const loginForm = document.getElementById("login");
 
 const handleLoginSubmit = (event) => {
-  // Prevent page from refreshing
-  event.preventDefault();
+	// Prevent page from refreshing
+	event.preventDefault();
 
-  // Extract e-mail and password from form
-  const email = loginForm.elements["email"].value;
-  const password = loginForm.elements["pwd"].value;
-  console.log(email, password);
+	// Extract e-mail and password from form
+	const email = loginForm.elements["email"].value;
+	const password = loginForm.elements["pwd"].value;
+	console.log(email, password);
 
-  // Create link to send post request to
-  const link =
-    "https://zi-webapp.azurewebsites.net/api/Login?" +
-    new URLSearchParams({
-      email: email,
-      password: password,
-    });
+	// Create link to send post request to
+	const link =
+		"https://zi-webapp.azurewebsites.net/api/Login?" +
+		new URLSearchParams({
+			email: email,
+			password: password,
+		});
 
-  // Create post request
-  fetch(link, {
-    method: "post",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // If the user does not exist, stop the function
-      if (data == -1) return;
+	// Create post request
+	fetch(link, {
+		method: "post",
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			// If the user does not exist, stop the function
+			if (data == -1) return;
 
-      // Set the uid (user ID) value to the user ID returned by the back=end
-      localStorage.setItem("uid", data);
+			// Set the uid (user ID) value to the user ID returned by the back=end
+			localStorage.setItem("uid", data);
 
-      window.location.href = "myaccount.html";
-    });
+			window.location.href = "myaccount.html";
+		});
 };
 
 loginForm.addEventListener("submit", handleLoginSubmit);
