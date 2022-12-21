cbd16108ab0aaae5557836763ad55074499b7788
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2022-12-21 13:12:59 +0100

    Improved currency and date formatting

--- invoicefetch.js
+++ invoicefetch.js
@@ -1,17 +1,22 @@
 const invoicesSum = (customerId) => {
-	fetch(`https://pythonbk.azurewebsites.net/api/sum_per_caretype/${customerId}`)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-			showSum(data.results);
-		});
+  fetch(`https://pythonbk.azurewebsites.net/api/sum_per_caretype/${customerId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      showSum(data.results);
+    });
 };
 
 function showSum(data) {
-	let sum = "";
+  let sum = "";
 
-	data.map((data) => {
-		sum += `
+  data.map((data) => {
+    data.total = new Intl.NumberFormat("nl-NL", {
+      style: "currency",
+      currency: "EUR",
+    }).format(data.total);
+
+    sum += `
     <tr>
     <td>
         ${data.caretype}
@@ -22,20 +27,20 @@ function showSum(data) {
     </div>
     </tr>
         `;
-	});
-	// Setting innerHTML as tab variable
-	document.getElementById("sumCare").innerHTML = sum;
+  });
+  // Setting innerHTML as tab variable
+  document.getElementById("sumCare").innerHTML = sum;
 }
 
 invoicesSum(localStorage.getItem("uid"));
 
 const invoicesfetch = (id) => {
-	fetch(`https://pythonbk.azurewebsites.net/api/invoices/${id}`)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-			show(data.results);
-		});
+  fetch(`https://pythonbk.azurewebsites.net/api/invoices/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      show(data.results);
+    });
 };
 
 // Calling that async function
@@ -43,10 +48,21 @@ invoicesfetch(localStorage.getItem("uid"));
 
 // Function to define innerHTML for HTML table
 function show(data) {
-	let code = "";
+  let code = "";
 
-	data.map((invoice) => {
-		code += `
+  data.map((invoice) => {
+    // Format data in invoice
+
+    // Format date
+    // invoice.date = new Date(invoice.date).toLocaleDateString("nl-NL");
+
+    // Format amount to an EUR value
+    invoice.amount = new Intl.NumberFormat("nl-NL", {
+      style: "currency",
+      currency: "EUR",
+    }).format(invoice.amount);
+
+    code += `
         <tr>
             <td>
                 ${invoice.invoice_nr}
@@ -55,15 +71,15 @@ function show(data) {
                 ${invoice.caretype}
             </td>
             <td>
-                ${invoice.invoice_date}
+                ${new Date(invoice.invoice_date).toLocaleDateString()}
             </td>
             <td>
                 ${invoice.amount}
             </td>
         </tr>
    `;
-	});
+  });
 
-	// Setting innerHTML as tab variable
-	document.getElementById("invoices").innerHTML = code;
+  // Setting innerHTML as tab variable
+  document.getElementById("invoices").innerHTML = code;
 }
